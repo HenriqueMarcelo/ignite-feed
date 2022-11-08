4fe1551b3f339764477297af077566bff44ae01f
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2022-11-08 17:05:49 -0300

    Creating Sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
 import {Post} from './Post';
 import {Header} from './components/Header';
+import {Sidebar} from './components/Sidebar';
 
 import './global.css'
+
+import styles from './App.module.css';
 
 function App() {
 
   return (
     <>
       <Header></Header>
-      <h1>Hello World!</h1>
-      <Post 
-        name="Ricardo" 
-        text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, a quisquam consectetur ipsam labore et rerum, in doloremque officiis vel architecto eum! Laudantium reprehenderit dolores sequi, culpa fugiat quaerat facilis?"/>  
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            name="Ricardo" 
+            text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, a quisquam consectetur ipsam labore et rerum, in doloremque officiis vel architecto eum! Laudantium reprehenderit dolores sequi, culpa fugiat quaerat facilis?"/>  
+          <Post 
+            name="Ricardo" 
+            text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, a quisquam consectetur ipsam labore et rerum, in doloremque officiis vel architecto eum! Laudantium reprehenderit dolores sequi, culpa fugiat quaerat facilis?"/>  
+          <Post 
+            name="Ricardo" 
+            text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, a quisquam consectetur ipsam labore et rerum, in doloremque officiis vel architecto eum! Laudantium reprehenderit dolores sequi, culpa fugiat quaerat facilis?"/>  
+          <Post 
+            name="Ricardo" 
+            text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, a quisquam consectetur ipsam labore et rerum, in doloremque officiis vel architecto eum! Laudantium reprehenderit dolores sequi, culpa fugiat quaerat facilis?"/>  
+        </main>
+      </div>
     </>
   )
 }
