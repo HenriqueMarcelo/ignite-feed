3d9890f0538feda76fb89938ec035186360a4d46
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2022-11-09 14:57:10 -0300

    Add eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
-import {Post} from './Post';
-import {Header} from './components/Header';
-import {Sidebar} from './components/Sidebar';
+import { Post } from './Post';
+import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
 
-import './global.css'
+import './global.css';
 
 import styles from './App.module.css';
 
 function App() {
-
   return (
     <>
-      <Header></Header>
+      <Header />
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post 
-            name="Ricardo" 
-            text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, a quisquam consectetur ipsam labore et rerum, in doloremque officiis vel architecto eum! Laudantium reprehenderit dolores sequi, culpa fugiat quaerat facilis?"/>  
-          <Post 
-            name="Ricardo" 
-            text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, a quisquam consectetur ipsam labore et rerum, in doloremque officiis vel architecto eum! Laudantium reprehenderit dolores sequi, culpa fugiat quaerat facilis?"/>  
-          <Post 
-            name="Ricardo" 
-            text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, a quisquam consectetur ipsam labore et rerum, in doloremque officiis vel architecto eum! Laudantium reprehenderit dolores sequi, culpa fugiat quaerat facilis?"/>  
-          <Post 
-            name="Ricardo" 
-            text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, a quisquam consectetur ipsam labore et rerum, in doloremque officiis vel architecto eum! Laudantium reprehenderit dolores sequi, culpa fugiat quaerat facilis?"/>  
+          <Post
+            name="Ricardo"
+            text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, a quisquam consectetur ipsam labore et rerum, in doloremque officiis vel architecto eum! Laudantium reprehenderit dolores sequi, culpa fugiat quaerat facilis?"
+          />
+          <Post
+            name="Ricardo"
+            text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, a quisquam consectetur ipsam labore et rerum, in doloremque officiis vel architecto eum! Laudantium reprehenderit dolores sequi, culpa fugiat quaerat facilis?"
+          />
+          <Post
+            name="Ricardo"
+            text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, a quisquam consectetur ipsam labore et rerum, in doloremque officiis vel architecto eum! Laudantium reprehenderit dolores sequi, culpa fugiat quaerat facilis?"
+          />
+          <Post
+            name="Ricardo"
+            text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, a quisquam consectetur ipsam labore et rerum, in doloremque officiis vel architecto eum! Laudantium reprehenderit dolores sequi, culpa fugiat quaerat facilis?"
+          />
         </main>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Post.jsx
+++ src/Post.jsx
@@ -1,8 +1,8 @@
-export function Post({name, text}) {
-    return (
-        <div>
-            <h3>{name}</h3>
-            <p>{text}</p>
-        </div>
-    )
+export function Post({ name, text }) {
+  return (
+    <div>
+      <h3>{name}</h3>
+      <p>{text}</p>
+    </div>
+  );
 }
